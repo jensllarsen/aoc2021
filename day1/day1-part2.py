2abcb5fbfcbf1e5180c6c7180de88dcaed534b6a
Jens Larsen <jens@98point6.com>
2021-12-05 13:47:03 -0800

    First try

--- day1/day1-part2.py
+++ day1/day1-part2.py
@@ -3,9 +3,24 @@ with open('input.txt') as f:
     measures = f.readlines()
     f.close()
 
-previous_measure = 9999 # hack to make sure the first measure isn't counted
-larger_measures = 0
+previous_sum = 9999999 # hack to make sure the first measure isn't counted
+larger_sums = 0
+index = 0
 
-for measure in measures:
+while( index < len(measures) - 3):
+    first = int(measures[index])
+    second = int(measures[index+1])
+    third = int(measures[index+2])
 
-print("Number of increases: " + str(larger_measures))
+    this_sum = first + second + third
+    print("This sum: " + str(this_sum))
+    print("Previous sum: " + str(previous_sum))
+
+    if this_sum > previous_sum:
+        larger_sums = larger_sums + 1
+        print("Bigger!")
+
+    previous_sum = this_sum # stash this sum for next iteration
+    index = index + 1
+
+print("Number of increases: " + str(larger_sums))
