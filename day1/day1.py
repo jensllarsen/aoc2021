24bf59a1b92393ab0b19d55964e89e8b476f8237
Jens Larsen <jens@98point6.com>
2021-12-05 13:03:21 -0800

    Add hack

--- day1/day1.py
+++ day1/day1.py
@@ -3,7 +3,7 @@ with open('input.txt') as f:
     measures = f.readlines()
     f.close()
 
-previous_measure = 0
+previous_measure = 9999 # hack to make sure the first measure isn't counted
 larger_measures = 0
 
 for measure in measures:
