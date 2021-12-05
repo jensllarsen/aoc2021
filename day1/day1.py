3d5c6fb28cd7b6e1936dcb6f418a280b8c294297
Jens Larsen <jens@98point6.com>
2021-12-05 13:00:04 -0800

    Add checking code

--- day1/day1.py
+++ day1/day1.py
@@ -3,5 +3,18 @@ with open('input.txt') as f:
     measures = f.readlines()
     f.close()
 
+previous_measure = 0
+larger_measures = 0
+
 for measure in measures:
-    print(measure)
+    this_measure = int(measure)
+    print("This measure: " + str(this_measure))
+    print("Previous measure: " + str(previous_measure))
+
+    if this_measure > previous_measure:
+        larger_measures = larger_measures + 1
+        print("Bigger!")
+
+    previous_measure = this_measure # store in previous_measure before starting with the next
+
+print("Number of increases: " + str(larger_measures))
